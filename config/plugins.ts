5e8315b68ac2a602d700c6d36bbe091422963665
Alexey T <rostovboy@gmail.com>
2024-11-17 21:16:13 +0300

    Users jwt-token expiration time

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+export default ({ env }) => ({
+  // https://docs.strapi.io/dev-docs/plugins/users-permissions
+  // expressed in seconds or a string describing a time span. Eg: 60, "45m", "10h", "2 days", "7d", "2y"
+  // default = 30d
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
+      },
+    },
+  },
+});
